c2f7a2bca23bed297701213d70e59e939ff90a2b
Luan Felipe <luanf2003@gmail.com>
2022-12-02 13:05:15 -0300

    Implement LinearGradientBrush

--- D2DControlers/Frame.cpp
+++ D2DControlers/Frame.cpp
@@ -1,4 +1,5 @@
 #include "Frame.h"
+#include "Exception.h"
 
 LRESULT Frame::HandleMessage(UINT uMsg, WPARAM wParam, LPARAM lParam)
 {
@@ -37,14 +38,48 @@ void Frame::OnPaint(ID2D1HwndRenderTarget* pRenderTarget)
 {
     PAINTSTRUCT ps;
     BeginPaint(hwnd, &ps);
-    pRenderTarget->FillRoundedRectangle(roundRect, pSolidColorBrush);
+
+    if(style.getTypeOfBrush() == SOLID_COLOR)
+        pRenderTarget->FillRoundedRectangle(roundRect, pSolidColorBrush);
+    else
+        pRenderTarget->FillRoundedRectangle(roundRect, pLinearGradientBrush);
     EndPaint(hwnd, &ps);
     
 }
 
 void Frame::CreateResources(ID2D1HwndRenderTarget* pRenderTarget)
 {
-    pRenderTarget->CreateSolidColorBrush(style.getBackgroundColors()[0], &pSolidColorBrush);
+    if(style.getTypeOfBrush() == SOLID_COLOR)
+        pRenderTarget->CreateSolidColorBrush(style.getBackgroundColors()[0], &pSolidColorBrush);
+    else if (style.getTypeOfBrush() == LINEAR_GRADIENT)
+    {
+        auto& vec = style.getBackgroundColors();
+        ID2D1GradientStopCollection* pGradientStops = nullptr;
+        D2D1_GRADIENT_STOP* gradientStop = (D2D1_GRADIENT_STOP*) malloc(sizeof(D2D1_GRADIENT_STOP) * vec.size());
+        if (gradientStop)
+        {
+            for (int i = 0; i < vec.size(); i++)
+            {
+                gradientStop[i].color = vec[i];
+                gradientStop[i].position = i / (vec.size() - 1);
+            }
+            pRenderTarget->CreateGradientStopCollection(gradientStop, vec.size(), D2D1_GAMMA_2_2, D2D1_EXTEND_MODE_CLAMP, &pGradientStops);
+            
+            HRESULT hr = pRenderTarget->CreateLinearGradientBrush(
+                D2D1::LinearGradientBrushProperties(
+                    D2D1::Point2(posX, posY),
+                    D2D1::Point2(posX + width, posY + height)
+                ),
+                pGradientStops,
+                &pLinearGradientBrush
+            );
+
+            if (!SUCCEEDED(hr))
+            {
+                ERROR_MESSAGE_LAST_ERROR();
+            }
+        }
+    }
     roundRect = D2D1::RoundedRect(D2D1::Rect(posX, posY, posX + width, posY + height), style.getCornerRadius(), style.getCornerRadius());
 }
 
--- D2DControlers/main.cpp
+++ D2DControlers/main.cpp
@@ -31,14 +31,18 @@ BOOL WinMain(HINSTANCE hInstance, HINSTANCE hIgnore, PSTR lpCmdLine, INT nCmdSho
 {
 	window = new Window("Teste", hInstance, WinProc, { RGB(255,255,255), RGB(0, 0, 0), RGB(0, 0, 0), RGB(0, 0, 0) });
 
-	ElementStyle style(0xFF0000, 0, 10, 0, 0);
+	//ElementStyle style(0xFF0000, 0, 10, 0, 0);
+	ElementStyle style(LINEAR_GRADIENT, { 0x451057, 0xa36110 }, 0, 0, 0);
 
-
-	Frame f(20, 20, 100, 100, ALIGN_CENTER, style);
+	Frame f(20, 20, 100, 100, ALIGN_STREACH, style);
 	rf = &f;
-	f.AddEvent(ON_MOUSE_HOVER, eventTest);
-	f.AddEvent(ON_MOUSE_HOVER_OUT, eventTestOut);
+	//f.AddEvent(ON_MOUSE_HOVER, eventTest);
+	//f.AddEvent(ON_MOUSE_HOVER_OUT, eventTestOut);
+
+	Frame f2(AUTO, AUTO, 50, 50, ALIGN_CENTER, ElementStyle(0x0, 0x0, 0, 0, 0));
+
 	window->AddElement(f);
+	window->AddElement(f2);
 	window->Show();
 
 }
